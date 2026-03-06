a0950909cf0cfb28e3d77bdfe7c2cc24226b345a
dmitrii-pov <popkovd.o@yandex.ru>
2026-03-06 15:31:46 +0300

    feat(db): add ffi connection stub for kphp

---
+++ src/Exception/NotImplementedException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LPhenom\Db\Exception;
+
+use LogicException;
+
+/**
+ * Thrown when a feature is not yet implemented.
+ */
+final class NotImplementedException extends LogicException
+{
+}
+
